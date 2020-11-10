9d9c07055c9fea6f82a134fb73bfdc089eccb4b1
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-10 22:17:19 +0530

    Added: To read or write the address book with persons contact as CSV file

---
+++ src/main/java/com/blz/addressbookcsv/json/AddressBook.java
@@ -0,0 +1,252 @@
+package com.blz.addressbookcsv.json;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+public class AddressBook {
+
+	static Scanner sc = new Scanner(System.in);
+	static List<PersonDetails> person = new ArrayList<PersonDetails>();
+	PersonDetails newEntry;
+	boolean isExist;
+
+	public void addContact(String addressBookName) {
+		isExist = false;
+		System.out.println("Enter your firstName : ");
+		String firstName = sc.nextLine();
+		System.out.println("Enter your lastName : ");
+		String lastName = sc.nextLine();
+		System.out.println("Enter your address : ");
+		String address = sc.nextLine();
+		System.out.println("Enter your city : ");
+		String city = sc.nextLine();
+		System.out.println("Enter your state : ");
+		String state = sc.nextLine();
+		System.out.println("Enter your zipCode : ");
+		String zip = sc.nextLine();
+		System.out.println("Enter your phoneNo : ");
+		String phoneNo = sc.nextLine();
+		System.out.println("Enter your emailId : ");
+		String email = sc.nextLine();
+		if (person.size() > 0) {
+			for (PersonDetails details : person) {
+				newEntry = details;
+				if (firstName.equals(newEntry.firstName) && lastName.equals(newEntry.lastName)) {
+					System.out.println("Contact " + newEntry.firstName + " " + newEntry.lastName + " already exists");
+					isExist = true;
+					break;
+				}
+			}
+		}
+		if (!isExist) {
+			newEntry = new PersonDetails(firstName, lastName, address, city, state, zip, phoneNo, email);
+			person.add(newEntry);
+			addDataToFile(firstName, lastName, address, city, state, phoneNo, zip, email, addressBookName);
+			try {
+				addDataToCSVFile(addressBookName);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void editContact() {
+		Scanner nameInput = new Scanner(System.in);
+		System.out.println(" Enter the first name ");
+		String fName = nameInput.nextLine();
+		for (int index = 0; index < person.size(); index++) {
+			if (person.get(index).getFirstName().equals(fName)) {
+				System.out.println(person.get(index));
+				Scanner input = new Scanner(System.in);
+				System.out
+						.println(" Enter a choice 	1.first name 2.last name 3. city 4.state 5.zip 6.phone 7.email ");
+				int option = nameInput.nextInt();
+				switch (option) {
+				case 1:
+					System.out.println(" Enter first name ");
+					String first_Name = input.nextLine();
+					person.get(index).setFirstName(first_Name);
+					System.out.println(person.get(index).getFirstName());
+					break;
+				case 2:
+					System.out.println(" Enter last name ");
+					String last_Name = input.nextLine();
+					person.get(index).setLastName(last_Name);
+					break;
+				case 3:
+					System.out.println(" Enter city name ");
+					String city = input.nextLine();
+					person.get(index).setCity(city);
+					break;
+				case 4:
+					System.out.println(" Enter State ");
+					String state = input.nextLine();
+					person.get(index).setCity(state);
+					break;
+				case 5:
+					System.out.println(" Enter pincode ");
+					String zip = input.nextLine();
+					person.get(index).setCity(zip);
+					break;
+				case 6:
+					System.out.println(" Enter Mobile number ");
+					String phone = input.nextLine();
+					person.get(index).setZip(phone);
+					break;
+				case 7:
+					System.out.println(" Enter Email id ");
+					String email = input.nextLine();
+					person.get(index).setCity(email);
+					break;
+				default:
+					System.out.println(" Enter valid input ");
+					break;
+				}
+			}
+		}
+		System.out.println(person);
+	}
+
+	public void deleteContact() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first name : ");
+		String firstName = sc.nextLine();
+		for (int i = 0; i < person.size(); i++) {
+			if (person.get(i).getFirstName().equalsIgnoreCase(firstName)) {
+				person.remove(i);
+			} else {
+				System.out.println("No data found");
+			}
+		}
+		sc.close();
+	}
+
+	public void searchByCity() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter city name : ");
+		String city = sc.nextLine();
+		person.stream().filter(n -> n.getCity().equals(city))
+				.forEach(i -> System.out.println("Data Found:" + i.getFirstName()));
+	}
+
+	public void viewByCity() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter city name : ");
+		String city = sc.nextLine();
+		person.stream().filter(n -> n.getCity().equals(city)).forEach(i -> System.out.println(i));
+	}
+
+	public void countBasedOnCity() {
+		int count = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter city name : ");
+		String city = sc.nextLine();
+		count = (int) person.stream().filter(n -> n.getCity().equals(city)).count();// count Stream;
+		System.out.println(count);
+	}
+
+	public void sortingByName() {
+		person = person.stream().sorted(Comparator.comparing(PersonDetails::getFirstName)).collect(Collectors.toList());
+		person.forEach(i -> System.out.println(i));
+	}
+
+	public void sortingByCity() {
+		person = person.stream().sorted(Comparator.comparing(PersonDetails::getCity)).collect(Collectors.toList());
+		person.forEach(i -> System.out.println(i));
+	}
+
+	public void addDataToFile(String firstName, String lastName, String address, String city, String state,
+			String phoneNumber, String zip, String email, String addressBookName) {
+		System.out.println("Enter name for text file : ");
+		String fileName = sc.nextLine();
+		File file = new File("C:\\Users\\Siva Reddy\\eclipse-workspace\\AddressBookCSV_Json" + fileName + ".txt");
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		try {
+			FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write("Contact:" + "\n1.First name: " + firstName + "\n2.Last name: " + lastName + "\n3.Address: "
+					+ address + "\n4.City: " + city + "\n5.State: " + state + "\n6.Phone number: " + phoneNumber
+					+ "\n7.Zip: " + zip + "\n8.email: " + email + "\n");
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void readDataFromFile() {
+		System.out.println("Enter address book filename : ");
+		String fileName = sc.nextLine();
+		Path filePath = Paths.get("C:\\Users\\Siva Reddy\\eclipse-workspace\\AddressBookCSV_Json" + fileName + ".txt");
+		try {
+			Files.lines(filePath).map(line -> line.trim()).forEach(line -> System.out.println(line));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void addDataToCSVFile(String addressBookName) throws IOException {
+		System.out.println("Enter name for csv file : ");
+		String fileName = sc.nextLine();
+		Path filePath = Paths.get("C:\\Users\\Siva Reddy\\eclipse-workspace\\AddressBookCSV_Json" + fileName + ".csv");
+
+		if (Files.notExists(filePath))
+			Files.createFile(filePath);
+		File file = new File(String.valueOf(filePath));
+
+		try {
+			FileWriter outputfile = new FileWriter(file, true);
+			CSVWriter writer = new CSVWriter(outputfile);
+			List<String[]> data = new ArrayList<>();
+			for (PersonDetails detail : person) {
+				data.add(new String[] { "Contact:" + "\n1.First name: " + detail.firstName + "\n2.Last name: "
+						+ detail.lastName + "\n3.Address: " + detail.address + "\n4.City: " + detail.city
+						+ "\n5.State: " + detail.state + "\n6.Phone number: " + detail.phoneNo + "\n7.Zip: "
+						+ detail.zip + "\n8.email: " + detail.email + "\n" });
+			}
+			writer.writeAll(data);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void readDataFromCSVFile() {
+		System.out.println("Enter address book CSV filename : ");
+		String fileName = sc.nextLine();
+		CSVReader reader = null;
+		try {
+			reader = new CSVReader(new FileReader(
+					"C:\\Users\\Siva Reddy\\eclipse-workspace\\AddressBookCSV_Json" + fileName + ".csv"));
+			String[] nextLine;
+			while ((nextLine = reader.readNext()) != null) {
+				for (String token : nextLine) {
+					System.out.println(token);
+				}
+				System.out.print("\n");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/main/java/com/blz/addressbookcsv/json/AddressBookMain.java
@@ -0,0 +1,147 @@
+package com.blz.addressbookcsv.json;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) throws IOException {
+		options();
+	}
+
+	static Map<String, AddressBookMain> addressBook = new HashMap<>();
+	static AddressBookMain mainObj = new AddressBookMain();
+	static Scanner sc = new Scanner(System.in);
+	static String addressBookName;
+
+	public void addAddressBook() throws IOException {
+		Scanner input = new Scanner(System.in);
+		System.out.println(
+				"Enter choice \n0.Creating new addressbook \n1.Adding contacts in existing register \n2.Exit ");
+		int entry = input.nextInt();
+		if (entry != 2) {
+			switch (entry) {
+			case 0:
+				Scanner nameInput = new Scanner(System.in);
+				System.out.println(" Enter name of address book ");
+				String nameOfNewBook = nameInput.nextLine();
+				if (addressBook.containsKey(nameOfNewBook)) {
+					System.out.println(" address book already exists");
+					break;
+				}
+				addressBook.put(nameOfNewBook, mainObj);
+				System.out.println(" address  book" + " " + nameOfNewBook + " " + "has been added");
+				AddressBookMain.options();
+				break;
+			case 1:
+				Scanner existingAddressName = new Scanner(System.in);
+				System.out.println(" Enter name of address book ");
+				String nameOfExistingRegister = existingAddressName.nextLine();
+				if (addressBook.containsKey(nameOfExistingRegister)) {
+					addressBook.get(nameOfExistingRegister);
+					AddressBookMain.options();
+				} else
+					System.out.println(" address book is not found ");
+			case 2:
+				entry = 2;
+				break;
+			default:
+				System.out.println(" Enter valid input ");
+				break;
+			}
+		}
+	}
+
+	public static void options() throws IOException {
+		AddressBook addressBook = new AddressBook();
+		Scanner sc = new Scanner(System.in);
+		int flag = 1;
+		while (flag == 1) {
+			System.out.println("Welcome to address book program ");
+			System.out.println(
+					"Enter choice \n1. AddContact \n2.Edit \n3.Delete \n4. Search \n5.View person \n6.Count no.of peoples in same city"
+							+ " \n7.SortByName \n8.SortByCityName \n9.AddContactToFile \n10.ReadDataFromFile \n11.addContactsToCSVFile \n12.ReadDataFromCSVFile"
+							+ " \n13. Exit ");
+			int choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+				addressBook.addContact(addressBookName);
+				break;
+			case 2:
+				if (AddressBook.person.isEmpty()) {
+					System.out.println(" Address book is empty ");
+					break;
+				}
+				addressBook.editContact();
+				break;
+			case 3:
+				if (AddressBook.person.isEmpty()) {
+					System.out.println(" Address book is empty ");
+					break;
+				}
+				addressBook.deleteContact();
+				break;
+			case 4:
+				if (addressBook.person.isEmpty()) {
+					System.out.println(" Address book is empty ");
+					break;
+				}
+				addressBook.searchByCity();
+				break;
+			case 5:
+				if (addressBook.person.isEmpty()) {
+					System.out.println(" Address book is empty ");
+					break;
+				}
+				addressBook.viewByCity();
+				break;
+			case 6:
+				if (addressBook.person.isEmpty()) {
+					System.out.println(" Address book is empty ");
+					break;
+				}
+				addressBook.countBasedOnCity();
+				break;
+			case 7:
+				if (addressBook.person.isEmpty()) {
+					System.out.println(" Address book is empty ");
+					break;
+				}
+				addressBook.sortingByName();
+				break;
+			case 8:
+				if (addressBook.person.isEmpty()) {
+					System.out.println(" Address book is empty ");
+					break;
+				}
+				addressBook.sortingByCity();
+				break;
+			case 9:
+				addressBook.addContact(addressBookName);
+				System.out.println("Successfully Added to text file");
+				break;
+			case 10:
+				addressBook.readDataFromFile();
+				break;
+			case 11:
+				addressBook.addContact(addressBookName);
+				System.out.println("Successfully Added to CSV file");
+				break;
+			case 12:
+				addressBook.readDataFromCSVFile();
+				break;
+			case 13:
+				mainObj.addAddressBook();
+				flag = 0;
+				break;
+			default:
+				System.out.println(" Enter a valid choice");
+				break;
+			}
+		}
+		System.out.println(addressBook.person);
+	}
+
+}
